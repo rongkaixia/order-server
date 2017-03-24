a6f83380f1b208ee4e3948de2bcbd2bc168c6872
rongkaixia@gmail.com <rongkaixia@gmail.com>
2017-03-24 12:38:12 +0800

    update code
    Former-commit-id: a7af6dcbecc4d78d6455ded82b407548d917b11a

--- src/containers/Promos/Promos.js
+++ src/containers/Promos/Promos.js
@@ -21,67 +21,105 @@ export default class Propagation extends Component {
     this.props.logout();
   }
 
-  // componentWillReceiveProps(nextProps) {
-  //   if (nextProps.location && nextProps.location !== this.currentLocation) {
-  //     // this.setState({previousLocation: nextProps.location});
-  //     this.previousLocation = this.currentLocation;
-  //     this.currentLocation = nextProps.location;
-  //   }
-  // }
-
   render() {
     const {user, location, cart} = this.props; // eslint-disable-line no-shadow
     const styles = require('./Promos.scss');
     return (
       <Grid className={styles.promoContainer}>
         <Row>
-          <Col xs={12} sm={6} md={6} lg={3} className={styles.promoColumn}>
+
+          <Col xs={12} sm={4} md={4} lg={4} className={styles.promoColumn}>
+            <Card onOver={card => card.setLevel(2)} onOut={card => card.setLevel(1)}
+            className={"promoCard"}>
+              <div className={styles.promoContentTop}>
+                <h6>项链</h6>
+                <p>精美绝伦，极致璀璨</p>
+              </div>
+              <figure className={styles.promoNecklace}/>
+            </Card>
+          </Col>
+
+          <Col xs={12} sm={4} md={4} lg={4} className={styles.promoColumn}>
             <Card onOver={card => card.setLevel(2)} onOut={card => card.setLevel(1)}
             className="promoCard">
               <div className={styles.promoContentTop}>
                 <h6>戒指</h6>
-                <p>项链吊坠有多重设计。搭配18k白金、黄金和双色项链来表达您的个人风格</p>
+                <p>翠璨翡翠搭配精致钻石与白金</p>
               </div>
               <figure className={styles.promoRing}/>
             </Card>
           </Col>
 
-          <Col xs={12} sm={6} md={6} lg={3} className={styles.promoColumn}>
+          <Col xs={12} sm={4} md={4} lg={4} className={styles.promoColumn}>
             <Card onOver={card => card.setLevel(2)} onOut={card => card.setLevel(1)}
-            className="promoCard">
-              <div className={styles.promoContentRight}>
-                <h6>耳环</h6>
-                <p>华贵白金与漂亮宝石是耳环的的组成元素</p>
-              </div>
-              <figure className={styles.promoEaring}/>
-            </Card>
-          </Col>
-          
-          <Col xs={12} sm={6} md={6} lg={3} className={styles.promoColumn}>
-            <Card onOver={card => card.setLevel(2)} onOut={card => card.setLevel(1)}
-            className={styles.promoNecklace}>
-              <div className={styles.promoContentTop}>
-                <h6 className={"inverse"}>项链</h6>
-                <p className={"inverse"}>精美绝伦，极致璀璨</p>
-              </div>
-              {/*<figure className={styles.promoNecklace}/>*/}
-            </Card>
-          </Col>
-
-          <Col xs={12} sm={6} md={6} lg={3} className={styles.promoColumn}>
-            <Card onOver={card => card.setLevel(2)} onOut={card => card.setLevel(1)}
-            className={styles.promoGift}>
+            className={"promoCard"}>
               <div className={styles.promoShadow}/>
               <div className={styles.promoContentTop}>
                 <h6 className={"inverse"}>礼品</h6>
                 <p className={"inverse"}>多样礼物，总有一款适合您</p>
               </div>
-              {/*<figure className={styles.promoOther}/>*/}
+              <figure className={styles.promoGift}/>
             </Card>
           </Col>
 
         </Row>
       </Grid>
     );
+  // render() {
+  //   const {user, location, cart} = this.props; // eslint-disable-line no-shadow
+  //   const styles = require('./Promos.scss');
+  //   return (
+  //     <Grid className={styles.promoContainer}>
+  //       <Row>
+  //         <Col xs={12} sm={6} md={6} lg={4} className={styles.promoColumn}>
+  //           <Col xs={7} sm={7} md={7} lg={7}className={styles.promoImage}>
+  //             <div className="promoCard">
+  //               <figure className={styles.promoNecklace}/>
+  //             </div>
+  //           </Col>
+  //           <Col xs={5} sm={5} md={5} lg={5}>
+  //             <div className={styles.promoContentCenter}>
+  //               <h6>戒指</h6>
+  //               <p>精美绝伦，极致璀璨</p>
+  //               <a>立即查看</a>
+  //             </div>
+  //           </Col>
+  //         </Col>
+
+
+  //         <Col xs={12} sm={6} md={6} lg={4} className={styles.promoColumn}>
+  //           <Col xs={7} sm={7} md={7} lg={7}className={styles.promoImage}>
+  //             <div className="promoCard">
+  //               <figure className={styles.promoRing}/>
+  //             </div>
+  //           </Col>
+  //           <Col xs={5} sm={5} md={5} lg={5}>
+  //             <div className={styles.promoContentCenter}>
+  //               <h6>项链</h6>
+  //               <p>翠璨翡翠搭配精致钻石与白金</p>
+  //               <a>立即查看</a>
+  //             </div>
+  //           </Col>
+  //         </Col>
+
+
+  //         <Col xs={12} sm={6} md={6} lg={4} className={styles.promoColumn}>
+  //           <Col xs={7} sm={7} md={7} lg={7}className={styles.promoImage}>
+  //             <div className="promoCard">
+  //               <figure className={styles.promoGift}/>
+  //             </div>
+  //           </Col>
+  //           <Col xs={5} sm={5} md={5} lg={5}>
+  //             <div className={styles.promoContentCenter}>
+  //               <h6>礼物</h6>
+  //               <p>多样礼物，总有一款适合您</p>
+  //               <a>立即查看</a>
+  //             </div>
+  //           </Col>
+  //         </Col>
+  //       </Row>
+  //     </Grid>
+  //   );
+  
   }
 }
